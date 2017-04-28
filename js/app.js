691b98e26d1265c8b2fc3d8e0ff413a868e39d23
yestian <yestian@qq.com>
2017-04-29 00:23:43 +0800

    create new files about agency platform unfinished

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 $(function() {
-    
+	winx=$(window).width();
+	winy=$(window).height();
+	//iframe auto height
+	$("#rightframe").height(document.documentElement.clientHeight+50);
+	//addClass to leftmenu when active
+	$(".leftmenu a").click(function(){
+		$(this).addClass("active")
+			   .siblings().removeClass("active");
+		$(this).parent().siblings().children().removeClass('active');
+	});
 });
